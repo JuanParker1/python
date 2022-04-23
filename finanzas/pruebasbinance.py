5427819202799e083ad0a223e2786a52abb048c2
pinipv <adrianpinillapf@gmail.com>
2022-04-23 23:30:00 +0200

    Update pruebasbinance.py

--- finanzas/pruebasbinance.py
+++ finanzas/pruebasbinance.py
@@ -6,8 +6,8 @@ from twisted.internet import reactor
 
 #Declaracion api_key y api_secret_key       Se recogen desde gestion de apis en mi cuenta binance
 
-kapi='i1C8P7g2qiGDpibFT8cE2YZGwC2lCgzyb6O3Gc7K3ojFIN58QJz3jU9ohHRmW0JU'
-sapi='r9XDXllkOwEf2t9PY25ootO7Ph5KC3lvSXnQxeMJmJnB0n1C90FhLVlPfAgFvERF'
+kapi=''
+sapi=''
 
 client=Client(kapi,sapi)
 shib_balance=client.get_asset_balance(asset='SHIB')
